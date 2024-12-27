9fd5bdc7e1d901d8114dcc12c31bc90651a1e5dd
Hycaro Phillype <hycarophillype0@gmail.com>
2024-12-27 09:29:44 -0300

    feat: Configuração de rotas ok.
    Primeiro passo ok!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent,} from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component'; 
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent, CriarPensamentosComponent,],
+  imports: [ RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentosComponent, ListarPensamentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'listarPensamento',
+        pathMatch: 'full',
+    },
+    {
+        path: 'criarPensamento',
+        component: CriarPensamentosComponent
+    },
+    {
+        path: 'listarPensamento',
+        component: ListarPensamentosComponent
+    }
+];
