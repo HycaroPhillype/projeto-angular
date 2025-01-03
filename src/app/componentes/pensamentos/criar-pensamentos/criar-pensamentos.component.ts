924e7af205510c39001bbe54b78cd2308b5dac60
Hycaro Phillype <hycarophillype0@gmail.com>
2025-01-03 15:39:04 -0300

    feat: Criando novos pensamentos
    Clicando no botão salvar, joga o novo pensamento para o arquivo db.json

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento/pensamento.service';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -13,17 +14,22 @@ import { Pensamento } from '../pensamento';
 export class CriarPensamentosComponent {
   
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo1'
   }
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private service:PensamentoService ) {}
 
   navegarParaListarPensamento() {
     this.router.navigate(['/listarPensamento'])
 
+
+  }
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 }
 
